f0d0f035f1789decf007a312746060328303f1f5
Jianing-Ju <m180141@e.ntu.edu.sg>
2021-02-15 20:03:57 +0800

    Changes for server

--- lns.py
+++ lns.py
@@ -79,7 +79,7 @@ def LNS_VRPTW(visit_fixed, time_fixed, distance, custs, vehicles, capacity, spee
 
     
     sol = mdl.solve(TimeLimit = time_lim, LogVerbosity = "Quiet", agent='local',
-               execfile='/Applications/CPLEX_Studio201/cpoptimizer/bin/x86-64_osx/cpoptimizer')
+               execfile='/home/hw1-a30/CPLEX_Studio1210/cpoptimizer/bin/x86-64_linux/cpoptimizer')
     # sol = mdl.solve(TimeLimit = 5, LogVerbosity = "Verbose", agent='local',
     #            execfile='/Applications/CPLEX_Studio201/cpoptimizer/bin/x86-64_osx/cpoptimizer')
     if not sol:
