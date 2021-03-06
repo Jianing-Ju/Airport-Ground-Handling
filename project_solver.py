8bab375075151fd11a7796c89f17f2a6945db461
Jianing-Ju <m180141@e.ntu.edu.sg>
2021-03-06 20:14:41 +0800

    New config

--- project_solver.py
+++ project_solver.py
@@ -166,7 +166,7 @@ if __name__ == '__main__':
 
     # Experiment configuration: (no of flights in an instance, no of instance)
     # possible number of flights: 20, 50, 100, 200, 300
-    experiments = [ (200, 5)]
+    experiments = [ (20, 50), (50, 50), (100, 20), (200, 20)]
     # experiments = [(20, 10)]
 
     for setting in experiments:
